c43026363d98b90f304a6b2e2a6cb70a642e54e1
esalling23 <esalling23@gmail.com>
2021-09-26 14:24:32 -0400

    Tweaks/randomizes planet animations
    Slight variations in speed, bounce, and rotation amount

--- src/components/Map/Planet.js
+++ src/components/Map/Planet.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from 'react'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
 import Spaceship from './Spaceship'
+import { randomNum } from '../../helpers'
 
 const PlanetContainer = styled(motion.div)`
     align-items: center;
@@ -24,38 +25,6 @@ const PlanetImg = styled(motion.img)`
     z-index: 0;
 `
 
-const planetVariants = {
-    bounce: {
-        y: [-5, 5],
-        rotate: [-5, 5],
-        transition: {
-            duration: 0.4,
-            yoyo: Infinity,
-            ease: "easeInOut",
-        }
-    },
-    spin: {
-        y: [0, -5, -15, -25, -30, -25, -15, -5, 0],
-        x: [0, 10, 15, 10, 0, -10, -15, -10, 0],
-        // rotate: [360, 0],
-        transition: {
-            repeat: Infinity,
-            duration: 0.8,
-            ease: "linear"
-        }
-    },
-    settle: {
-        y: [0, 5, 0],
-        rotate: [-5, 5],
-        transition: {
-            repeat: Infinity,
-            repeatType: 'reverse',
-            duration: 2,
-            ease: "easeInOut"
-        }
-    }
-}
-
 const Planet = ({
     img,
     handleClick,
@@ -67,9 +36,54 @@ const Planet = ({
     // Only handle the click once - either on click or tap
     const [tapped, setTapped] = useState(false)
 
+    const planetVariants = {
+        bounce: {
+            y: [-5, 5],
+            rotate: [-15, 15],
+            transition: {
+                duration: 0.8,
+                repeatType: 'reverse',
+                repeat: Infinity,
+                ease: "easeInOut",
+            }
+        },
+        circle: {
+            y: [0, -5, -15, -25, -30, -25, -15, -5, 0],
+            x: [0, 10, 15, 10, 0, -10, -15, -10, 0],
+            // rotate: [360, 0],
+            transition: {
+                repeat: Infinity,
+                duration: 0.8,
+                ease: "linear"
+            }
+        },
+        settle: {
+            y: [0, randomNum(10), 0],
+            rotate: [randomNum(-5), randomNum(10)],
+            transition: {
+                repeat: Infinity,
+                repeatType: 'reverse',
+                duration: 2,
+                ease: "easeInOut",
+                delay: randomNum(3)
+            }
+        },
+        zoom: {
+            scale: 1.2,
+            transition: {
+                duration: 0.6,
+                ease: "backInOut"
+            }
+        }
+    }
+
     const animation = useCallback(() => {
-        return isSelected ? 'bounce' : hover ? 'spin' : 'settle'
+        return isSelected ? 'bounce' : hover ? 'circle' : 'settle'
     }, [isSelected, hover])
+
+    const baseAnimation = useCallback(() => {
+        return isSelected ? 'zoom' : 'settle'
+    }, [isSelected])
 
     return (
         <PlanetContainer
@@ -82,7 +96,6 @@ const Planet = ({
                 handleClick(e)
             }}
             onMouseEnter={e => {
-                console.log('mouse entered')
                 if (!isSelected) {
                     setHover(true)
                 }
@@ -95,7 +108,7 @@ const Planet = ({
                 src={img}
                 variants={planetVariants}
                 initial='settle'
-                animate={name === 'base' ? 'settle' : animation()}
+                animate={name === 'base' ? baseAnimation() : animation()}
             />
             {isSelected && (
                 <Spaceship />
