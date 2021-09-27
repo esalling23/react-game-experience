9d536b885cddeeabb4fce2e76db962fbb65fce2f
esalling23 <esalling23@gmail.com>
2021-09-26 23:04:14 -0400

    Adds spaceship sounds, tilt to flying animation

--- src/components/Map/Spaceship.js
+++ src/components/Map/Spaceship.js
@@ -1,6 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo, useEffect } from 'react'
 import { motion } from 'framer-motion'
+import { Howl } from 'howler'
 import styled from 'styled-components'
+import { randomNum } from '../../helpers'
+
+const soundDir = '/audio/sfx/spaceship/'
+const flyingSoundPaths = [
+    'future-engine.wav',
+    'machine-glitch.wav',
+    'traveling.wav',
+    'time-machine.wav',
+    'vibrating-engine.wav'
+]
+
+const flyingSounds = flyingSoundPaths.map(s => `${soundDir}${s}`)
 
 const spaceshipImg = '/images/ufo.png'
 
@@ -14,7 +27,8 @@ const BouncingShip = styled(motion.img)`
 
 const spaceshipVariants = {
     flying: {
-        y: [0, 20, 0]
+        y: [0, 20, 0],
+        rotate: [0, 5, 20, 10, 0]
     },
     landed: {
         y: [0, 5, 0],
@@ -25,8 +39,22 @@ const spaceshipVariants = {
 }
 
 const Spaceship = () => {
+    const engineSound = useMemo(() => new Howl({
+        src: [flyingSounds[randomNum(flyingSounds.length - 1)]],
+        volume: 2.5
+    }), [])
     const [isFlying, setIsFlying] = useState(true)
-    console.log('spaceship is flying: ' + isFlying)
+
+    useEffect(() => {
+        if (isFlying) {
+            engineSound.play()
+            engineSound.fade(0, 1, 400)
+        } else {
+            engineSound.fade(engineSound.volume(), 0, 400)
+            engineSound.once('fade', () => engineSound.stop())
+        }
+    }, [engineSound, isFlying])
+
     return (
         <BouncingShip
             layoutId="spaceship"
