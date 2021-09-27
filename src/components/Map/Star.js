a0db9867a73762a0a318474bdb8330974eb68d32
esalling23 <esalling23@gmail.com>
2021-09-26 21:59:16 -0400

    Adds star shooting sounds
    Only plays sounds while star is visible within star container
    Uses IntersectionObserver API

--- src/components/Map/Star.js
+++ src/components/Map/Star.js
@@ -1,34 +1,54 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react'
 import styled from 'styled-components'
+import { Howl } from 'howler'
 import { motion, useAnimation } from 'framer-motion'
+
+import { randomNum } from '../../helpers'
 
 const smallStar = '/images/star_small.png'
 const mediumStar = '/images/star_medium.png'
 
+const soundDir = '/audio/sfx/stars/'
+const soundPaths = [
+    'fairy-swoosh.wav',
+    'fire-swoosh.wav',
+    'future-swoosh.wav',
+    'sweeping-swoosh.wav'
+]
+
+const shootingSounds = soundPaths.map(s => `${soundDir}shooting/${s}`)
+
 const FlyingStar = styled(motion.img)`
-    height: 5em;
+    height: 2.5em;
     left: ${props => props.leftOffset};
     position: absolute;
     top: ${props =>  props.topOffset};
-    width: 5em;
+    width: 2.5em;
     z-index: 10;
 `
 
 const CATCH_ANIM_TIME = 0.6
 
-const Star = ({ top, left, destroy, destroyTime, size }) => {
+const Star = ({ containerRef, top, left, destroy, destroyTime, size }) => {
     const controls = useAnimation()
     const starRef = useRef(null)
+    const destroyTimeout = useRef(null)
     const [isShooting, setIsShooting] = useState(false)
     const [isCaught, setIsCaught] = useState(false)
+    const [soundPlayed, setSoundPlayed] = useState(false)
+    const shootSfx = useMemo(() => new Howl({
+        src: shootingSounds[randomNum(shootingSounds.length - 1)],
+        rate: 0.25,
+        volume: 0.15,
+    }), [])
 
     const starVariants = {
         shooting: {
             rotate: 360,
-            opacity: ['0%', '60%', '100%', '60%', '0%'],
-            times: [0, 0.4, 0.5, 0.8, 1],
-            x: [-100, window.innerWidth + 500],
-            y: [-100, window.innerHeight + 500],
+            opacity: ['0%', '80%', '100%', '80%', '0%'],
+            times: [0, 0.1, 0.5, 0.8, 1],
+            x: window.innerWidth,
+            y: window.innerHeight,
             transition: {
                 duration: destroyTime
             }
@@ -42,12 +62,44 @@ const Star = ({ top, left, destroy, destroyTime, size }) => {
         }
     }
 
+    const options = useMemo(() => ({
+        root: containerRef.current,
+        rootMargin: '0px',
+        threshold: 1.0
+    }), [containerRef])
+
+    // Determines if star is visible & plays sfx
+    const checkStarVisible = useCallback((entries) => {
+        const [entry] = entries
+
+        if (entry.isIntersecting && !soundPlayed && starRef.current) {
+            shootSfx.play()
+            setSoundPlayed(true)
+        }
+    }, [soundPlayed, starRef, shootSfx])
+
+    useEffect(() => {
+        const star = starRef.current
+        const observer = new IntersectionObserver(checkStarVisible, options)
+
+        if (star) {
+            observer.observe(star)
+        }
+
+        return () => {
+            if (star) observer.unobserve(star)
+        }
+    }, [starRef, checkStarVisible, options])
+
+    // Handles shooting vs caught animations
+    // Stars are destroyed after caught & destroyTimeout is cleared
     useEffect(() => {
         if (isCaught) {
             controls.stop()
             controls.start('caught')
                 .then(() => {
                     destroy()
+                    clearTimeout(destroyTimeout.current)
                 })
         } else {
             if (!isShooting) {
@@ -55,9 +107,9 @@ const Star = ({ top, left, destroy, destroyTime, size }) => {
                 controls.start('shooting')
                     .then(() => {
                         destroy()
+                        clearTimeout(destroyTimeout.current)
                     })
             }
-
         }
     }, [isCaught, controls, destroy, isShooting])
 
@@ -67,7 +119,9 @@ const Star = ({ top, left, destroy, destroyTime, size }) => {
             destroy()
         }, destroyTime * 1000)
 
-        return () => clearTimeout(destroyTimeout.current)
+        return function () {
+            clearTimeout(destroyTimeout.current)
+        }
     }, [destroyTime, destroy])
 
     return (
